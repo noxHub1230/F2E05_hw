236c89e7b610ea9c00db55672832feb05d44a02f
kyw <35fghu@gmail.com>
2026-09-18 12:27:06 +0800

    chore：新增檢視模式切換功能

--- tabCtrl.js
+++ tabCtrl.js
@@ -131,26 +131,53 @@ const homework=[
     `
  }
 ];
-const content=document.querySelector("#content");
-const nav=document.querySelector("nav");
+const content = document.querySelector("#content");
+const nav = document.querySelector("nav");
 
-nav.addEventListener('click',(event)=>{
-    const selectedDate=event.target;
-    //給選中按鈕設定樣式控制
-    const activedBtn=document.querySelector("nav button.active");
-    if (activedBtn){
-        activedBtn.classList.remove("active");
-    }
-    selectedDate.classList.add("active");
-    //切換和找資料
-    const targetWork=homework.find(item=>item.date===selectedDate.dataset.date);
-    if(targetWork){
-    content.innerHTML=targetWork.html;
-    content.style.background = "#ecc692";
+let isSwitch = false;
+
+function renderWork(targetWork) {
+    //將標籤字元轉換成不會被解析渲染的字元代碼
+    const srcCode = `<pre style="color:#000">${targetWork.html
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+    }</pre>`;
+
+    content.innerHTML = `
+        <div>
+            <button class="switchBtn">
+                點擊切換渲染結果/原始碼
+            </button>
+            ${!isSwitch ? targetWork.html : srcCode}
+        </div>
+    `;//顯示渲染結果或原始碼
+
+    const switchBtn = document.querySelector(".switchBtn");
+
+    switchBtn.addEventListener("click", () => {
+        isSwitch = !isSwitch;
+        renderWork(targetWork);//使每次點擊都觸發重渲染(瀏覽器會在DOM變動時重渲染)
+    });
+}
+
+nav.addEventListener("click", event => {
+    const selectedBtn = event.target;//被點擊的那一個按鈕
+    const activeBtn = document.querySelector("nav button.active");//選取所有帶.active的button
+    if (activeBtn) {
+        activeBtn.classList.remove("active");
+    }//清空所有.active按鈕的.active
+
+    selectedBtn.classList.add("active");//給選中按鈕設成.active樣式
+    const targetWork = homework.find(
+        item => item.date === selectedBtn.dataset.date
+    );//在homeWork裡面找到對應日期的obj並回傳給targetWork(obj)
+
+    if (targetWork) {
+        isSwitch = false;
+        renderWork(targetWork);
+        content.style.background = "#ecc692";
     } else {
         content.innerHTML = "<p>~ 這裡空空如也 ~</p>";
-    }    
+    }
 });
-
-
-
