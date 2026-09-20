a28da84c7f67933dd9f09844cc8ac02282fa570f
“名字” <>
2026-09-20 19:41:46 +0800

    feat：添加樣式與資料

--- tabCtrl.js
+++ tabCtrl.js
@@ -1,3 +1,5 @@
+import { generateData } from "./0918_data.js";
+
 const homework=[
  {
     date:"909",
@@ -127,21 +129,21 @@ const homework=[
     date:"918",
     html:`
     <div id="sepEighteenth">
-        <h1>個人報名 Individual Registration</h1>
+        <h1 class="groupTitle mainTitle">個人報名 Individual Registration</h1>
         <form>
-            <fieldset>
-            <legend>基本資料 / Basic Information</legend>
+            <fieldset class="group">
+            <legend class="groupTitle">基本資料 / Basic Information</legend>
                 <label for="name">姓名 / Name</label>
                     <input id="name" type="text" name="name" placeholder="請輸入姓名" required/>
                 <fieldset>
-                    <legend>性別 / Gender</legend>
+                    <legend class="subTitle">性別 / Gender</legend>
                     <input id="male" type="radio" name="gender" value="male">
                     <label for="male">男 / Male</label>
                     <input id="female" type="radio" name="gender" value="female">
                     <label for="female">女 / Female</label>
                 </fieldset>
                 <fieldset>
-                    <legend>國籍 / Nationality</legend>
+                    <legend class="subTitle">國籍 / Nationality</legend>
                     <input id="roc" type="radio" name="nationality" value="roc"/>
                     <label for="roc">本國國籍 / ROC Citizens</label>
                     <input id="non" type="radio" name="nationality" value="non"/>
@@ -150,7 +152,7 @@ const homework=[
                 <label for="id">身分證號 / ID Number</label>
                     <input id="id" type="text" name="id" placeholder="身分證或護照號碼" required/>
                 <fieldset>
-                    <legend>出生日期 / Date Of Birth</legend>
+                    <legend class="subTitle">出生日期 / Date Of Birth</legend>
                     <select id="year" name="birthYear">
                         <option disabled selected>年 / Year</option>
                         <!--使用JS動態產生opt-->
@@ -167,7 +169,7 @@ const homework=[
                 <label for="email">電子信箱 / Email</label>
                     <input id="email" type="email" name="email" placeholder="example@mail.com" required/>
                 <fieldset>
-                    <legend>聯絡電話 / Contact Number</legend>
+                    <legend class="subTitle">聯絡電話 / Contact Number</legend>
                     <label for="mob">手機 / Mobile</label>
                         <input id="mob" type="tel" name="mob" placeholder="0922053012"/>
                     <label for="tel">住家 / Home</label>
@@ -176,13 +178,13 @@ const homework=[
                         <input id="com" type="tel" name="com"/>
                 </fieldset>
                 <fieldset>
-                    <legend>通訊地址 / Mailing Address</legend>
+                    <legend class="subTitle">通訊地址 / Mailing Address</legend>
                     <select id="cty" name="cty">
-                        <option value="" selected>請選擇縣市別 County/City</option>
+                        <option value="" disabled selected>請選擇縣市別 County/City</option>
                         <!--使用JS動態產生opt-->
                     </select>    
                     <select id="town" name="town">
-                        <option value="" selected>請選擇鄉鎮區別 Township</option>
+                        <option value="" disabled selected>請選擇鄉鎮區別 Township</option>
                         <!--使用JS動態產生opt-->
                     </select>  
                     <input id="mailId" type="text" name="mailId" inputmode="numeric" placeholder="郵遞區號"/>     
@@ -197,8 +199,8 @@ const homework=[
                         <input id="phone" type="tel" name="phone"/>     
                 </fieldset>            
             </fieldset>
-            <fieldset>
-            <legend>競賽項目資料 / Race Category Information</legend>
+            <fieldset class="group">
+            <legend class="groupTitle">競賽項目資料 / Race Category Information</legend>
                 <fieldset>
                     <input id="9km" type="checkbox" name="distance" value="9km"/>
                     <label for="9km">9KM (NTD 800)</label>
@@ -210,14 +212,14 @@ const homework=[
                 <fieldset>
                     <input id="3km" type="checkbox" name="distance" value="3km"/>
                     <label for="3km">3KM (NTD 650)</label>
-                    <select id="3km_size" name="9km_size">
+                    <select id="3km_size" name="3km_size">
                         <option value="">請選擇衣服尺寸</option>
                         <!--使用JS動態產生元素-->
                     </select>
                 </fieldset>
             </fieldset>
-            <fieldset>
-                <legend>報到方式(二擇一) / Check-in Method</legend>
+            <fieldset class="group">
+                <legend class="groupTitle">報到方式(二擇一) / Check-in Method</legend>
                 <label>郵寄代理報到 / Postal delivery check-in</label>
                 <input id="Y" type="radio" name="posDelivery" value="Y"/>
                     <label for="Y">是 / Y (需額外付費，限台灣本島)</label>
@@ -231,17 +233,23 @@ const homework=[
                     <input id="endDate" type="date" name="endDate"/>
                 </fieldset>
             </fieldset>
-            <fieldset>
-                <legend>其他 / Others</legend>
-                <label for="website">個人網站 / Website (選填)</label>
-                <input id="website" type="url" name="website" placeholder="https://"/>
-                <label for="note">備註 / Note</label>
-                <textarea id="note" name="note" placeholder="如有特殊需求請於此說明" rows="4" cols="15"></textarea>
-                <label for="pic">上傳證件照 / Upload Photo</label>
-                <input id="pic" type="file" accept="image/*"/> 
+            <fieldset class="group">
+                <legend class="groupTitle">其他 / Others</legend>
+                <div class="container">
+                    <label for="website">個人網站 / Website (選填)</label>
+                    <input id="website" type="url" name="website" placeholder="https://"/>
+                </div>
+                <div class="container">
+                    <label for="note">備註 / Note</label>
+                    <textarea id="note" name="note" placeholder="如有特殊需求請於此說明" rows="4" cols="15"></textarea>
+                </div>
+                <div class="container">
+                    <label for="pic">上傳證件照 / Upload Photo</label>
+                    <input id="pic" type="file" accept="image/*"/> 
+                </div>
             </fieldset>
-            <button type="submit">送出報名</button>
-            <button type="reset">清除重填</button>
+            <button id="submitBtn" type="submit">送出報名</button>
+            <button id="resetBtn" type="reset">清除重填</button>
         </form>
     </div>
     `
@@ -268,6 +276,9 @@ function renderWork(targetWork) {
             ${!isSwitch ? targetWork.html : srcCode}
         </div>
     `;//顯示渲染結果或原始碼
+    if(targetWork.date==="918"&& !isSwitch){
+        generateData();
+    }
 
     const switchBtn = document.querySelector(".switchBtn");
 
