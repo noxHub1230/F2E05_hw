3d294d7182962a41d6fd3ecf9606d52e38105b01
“名字” <>
2026-09-20 14:40:50 +0800

    chore: 表單html架構完成

--- tabCtrl.js
+++ tabCtrl.js
@@ -128,66 +128,110 @@ const homework=[
     html:`
     <div id="sepEighteenth">
         <h1>個人報名 Individual Registration</h1>
-        <section>
-        <h2>基本資料 / Basic Information</h2>
+        <fieldset>
+        <legend>基本資料 / Basic Information</legend>
             <label for="name">姓名 / Name</label>
-                <input id="name" type="text" name="name" placeholder="請輸入姓名" required></input>
+                <input id="name" type="text" name="name" placeholder="請輸入姓名" required/>
             <label>性別 / Gender</label>
-                <input id="male" type="radio" name="gender" value="male"></input>
+                <input id="male" type="radio" name="gender" value="male"/>
                 <label for="male">男 /Male</label>
-                <input id="female" type="radio" name="gender" value="female"></input>
+                <input id="female" type="radio" name="gender" value="female"/>
                 <label for="female">女 /Female</label>
             <label>國籍 / Nationality</label>
-                <input id="roc" type="radio" name="nationality" value="roc"></input>
+                <input id="roc" type="radio" name="nationality" value="roc"/>
                 <label for="roc">本國國籍 / ROC Citizens</label>
-                <input id="non" type="radio name="nationality" value="non"></input>
+                <input id="non" type="radio" name="nationality" value="non"/>
                 <label for="non">外國國籍 / Non-Citizens</label>
             <label for="id">身分證號 / ID Number</label>
-                <input id="id" type="text" name="id" placeholder="身分證或護照號碼" required></input>
-            <label for="birthDate">出生日期 / Date Of Birth</label>
-                <select id="year">
+                <input id="id" type="text" name="id" placeholder="身分證或護照號碼" required/>
+            <fieldset>
+                <legend>出生日期 / Date Of Birth</legend>
+                <select id="year" name="birthYear">
                     <option disabled selected>年 / Year</option>
                     <!--使用JS動態產生opt-->
                 </select>
-                <select id="month">
+                <select id="month" name="birthMonth">
                     <option disabled selected>月 / Month</option>
                     <!--使用JS動態產生opt-->
                 </select>
-                <select id="day">
+                <select id="day" name="birthDay">
                     <option disabled selected>日 / Day</option>
                     <!--使用JS動態產生opt-->
                 </select>
-            <label for="email" required>電子信箱 / Email</label>
-                <input id="email" type="email" name="email" placeholder="examplemail.com"></input>
+            </fieldset>
+            <label for="email">電子信箱 / Email</label>
+                <input id="email" type="email" name="email" placeholder="examplemail.com" required/>
             <label>聯絡電話 / Contact Number</label>
                 <label for="mob">手機 / Mobile</label>
-                    <input id="mob" type="number" name="mob" placeholder="0922053012"></input>
+                    <input id="mob" type="tel" name="mob" placeholder="0922053012"/>
                 <label for="tel">住家 / Home</label>
-                    <input id="tel" type="number" name="tel" placeholder="0225855659"></input>
+                    <input id="tel" type="tel" name="tel" placeholder="0225855659"/>
                 <label for="com">公司 /Office</label>
-                    <input id="com" type="number" name="tel></input>
+                    <input id="com" type="tel" name="tel"/>
             <label for="address">通訊地址 / Mailing Address</label>
                 <select id="cty">
-                    <option value="" selected>請選擇縣市別 County/City</value>
+                    <option value="" selected>請選擇縣市別 County/City</option>
                     <!--使用JS動態產生opt-->
                 </select>    
-                <select id="cty">
-                    <option value="" selected>請選擇縣市別 County/City</value>
+                <select id="town">
+                    <option value="" selected>請選擇鄉鎮區別 Township</option>
                     <!--使用JS動態產生opt-->
                 </select>  
-                <input id="mailId" type="number" name="mailId" placeholder="郵遞區號"></input>     
-                <input id="location" type="text" name="location" placeholder="例如：昌吉街55號2樓206室"></input> 
+                <input id="mailId" type="text" name="mailId" inputmode="numeric" placeholder="郵遞區號"/>     
+                <input id="location" type="text" name="location" placeholder="例如：昌吉街55號2樓206室"/> 
             <label for="emgcyContact">緊急聯絡人姓名 / Name of Emergency Contact</label>
-                <input id="emgcyContact" type="text" name="emgcyContact"></input>
+                <input id="emgcyContact" type="text" name="emgcyContact"/>
             <label for="ship">關係 / Relationship</label>
-                <input id="ship" type="text" name="ship"></input> 
+                <input id="ship" type="text" name="ship"/> 
             <label for="phone">電話 / Phone</label>
-                <input id="phone" type="number" name="phone"></input>     
-        </section>
-        <section>
-        <h2>競賽項目資料 / Race Category Information</h2>
-        </section>
-    <div>
+                <input id="phone" type="number" name="phone"/>     
+        </fieldset>
+        <fieldset>
+        <legend>競賽項目資料 / Race Category Information</legend>
+            <fieldset>
+                <input id="9km" type="checkbox" name="distance" value="9km"/>
+                <label for="9km">9KM (NTD 800)</label>
+                <select id="9km_size">
+                    <option value="">請選擇衣服尺寸</option>
+                    <!--使用JS動態產生元素-->
+                </select>
+            </fieldset>
+            <fieldset>
+                <input id="3km" type="checkbox" name="distance" value="3km"/>
+                <label for="3km">3KM (NTD 650)</label>
+                <select id="3km_size">
+                    <option value="">請選擇衣服尺寸</option>
+                    <!--使用JS動態產生元素-->
+                </select>
+            </fieldset>
+        </fieldset>
+        <fieldset>
+            <legend>報到方式(二擇一) / Check-in Method</legend>
+            <label>郵寄代理報到 / Postal delivery check-in</label>
+            <input id="Y" type="radio" name="posDelivery" value="Y"/>
+                <label for="Y">是 / Y (需額外付費，限台灣本島)</label>
+            <input id="N" type="radio" name="posDelivery" value="N"/>
+                <label for="N">否 / N </label>
+            <fieldset>
+                <input id="tpe1" type="radio" name="checkinPlace" value="tpe1"/>
+                <label>地點：花博爭豔館 (臺北市中山區玉門街1號)</label>
+                <span>時間：</span>
+                <input id="beginDate" type="date" name="beginDate"/>
+                <input id="endDate" type="date" name="endDate"/>
+            </fieldset>
+        </fieldset>
+        <fieldset>
+            <legend>其他 / Others</legend>
+            <label for="website">個人網站 / Website (選填)</label>
+            <input id="website" type="text" name="website" placeholder="https://"/>
+            <label for="note">備註 / Note</label>
+            <textarea id="note" name="note" placeholder="如有特殊需求請於此說明" rows="4" cols="15"></textarea>
+            <label for="pic">上傳證件照 / Upload Photo</label>
+            <input id="pic" type="file" accept="image/*"/> 
+        </fieldset>
+        <button type="submit">送出報名</button>
+        <button type="reset">清除重填</button>
+    </div>
     `
  }
 ];
