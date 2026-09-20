aca9c34640a6d6884d5ade47f5d644896e31976d
“名字” <>
2026-09-20 19:50:08 +0800

    refactor：模組化架構

--- js/srcCodes.js
+++ js/srcCodes.js
@@ -1,5 +1,3 @@
-import { generateData } from "./0918_data.js";
-
 const homework=[
  {
     date:"909",
@@ -255,56 +253,4 @@ const homework=[
     `
  }
 ];
-const content = document.querySelector("#content");
-const nav = document.querySelector("nav");
-
-let isSwitch = false;
-
-function renderWork(targetWork) {
-    //將標籤字元轉換成不會被解析渲染的字元代碼
-    const srcCode = `<pre style="color:#000">${targetWork.html
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-    }</pre>`;
-
-    content.innerHTML = `
-        <div>
-            <button class="switchBtn">
-                點擊切換渲染結果/原始碼
-            </button>
-            ${!isSwitch ? targetWork.html : srcCode}
-        </div>
-    `;//顯示渲染結果或原始碼
-    if(targetWork.date==="918"&& !isSwitch){
-        generateData();
-    }
-
-    const switchBtn = document.querySelector(".switchBtn");
-
-    switchBtn.addEventListener("click", () => {
-        isSwitch = !isSwitch;
-        renderWork(targetWork);//使每次點擊都觸發重渲染(瀏覽器會在DOM變動時重渲染)
-    });
-}
-
-nav.addEventListener("click", event => {
-    const selectedBtn = event.target;//被點擊的那一個按鈕
-    const activeBtn = document.querySelector("nav button.active");//選取所有帶.active的button
-    if (activeBtn) {
-        activeBtn.classList.remove("active");
-    }//清空所有.active按鈕的.active
-
-    selectedBtn.classList.add("active");//給選中按鈕設成.active樣式
-    const targetWork = homework.find(
-        item => item.date === selectedBtn.dataset.date
-    );//在homeWork裡面找到對應日期的obj並回傳給targetWork(obj)
-
-    if (targetWork) {
-        isSwitch = false;
-        renderWork(targetWork);
-        content.style.background = "#ecc692";
-    } else {
-        content.innerHTML = "<p>~ 這裡空空如也 ~</p>";
-    }
-});
+export {homework};
